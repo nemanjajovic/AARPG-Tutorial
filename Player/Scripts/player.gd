54f18b603bf5f08bb3cf94eb3414becb98aa97e5
Nemanja Jovic <nemanja.jovic195@gmail.com>
2026-07-06 15:20:14 +0200

    Added player animations for up,down,left,right and implamented them into movement

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,13 +1,59 @@
 class_name Player extends CharacterBody2D
 
 var move_speed : float = 100.0
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
+var state : String = "idle"
 
-func _process(delta: float) -> void:
-	var direction : Vector2 = Vector2.ZERO
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+
+
+func _ready() -> void:
+	UpdateAnimation()
 	
+	
+func _physics_process(_delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * move_speed
 	
-func _physics_process(delta: float) -> void:
+	if SetState() or SetDirection():
+		UpdateAnimation()
 	move_and_slide()
+
+func SetDirection() -> bool:
+	var new_dir : Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+	
+	if direction.y == 0:
+		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	
+	if new_dir == cardinal_direction:
+		return false
+	
+	cardinal_direction = new_dir
+	
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+	return true
+	
+
+func SetState() -> bool:
+	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state:
+		return false
+	state = new_state
+	return true
+	
+func UpdateAnimation() -> void:
+	animation_player.play(state + "_" + AnimDirection())
+
+func AnimDirection() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
