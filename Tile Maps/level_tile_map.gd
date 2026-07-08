f5871989a6da3510ce0789b5c7c74f0d699df626
Nemanja Jovic <nemanja.jovic195@gmail.com>
2026-07-08 17:37:57 +0200

    Implemented camera bounds

---
+++ Tile Maps/level_tile_map.gd
@@ -0,0 +1,15 @@
+class_name LevelTileMap extends TileMapLayer
+
+func _ready() -> void:
+	LevelManager.ChangeTileMapBounds(GetTilemapBounds())
+
+func GetTilemapBounds() -> Array[Vector2]:
+	var bounds : Array[Vector2] = []
+	bounds.append(
+		Vector2(get_used_rect().position * rendering_quadrant_size)
+	)
+	bounds.append(
+		Vector2(get_used_rect().end * rendering_quadrant_size)
+	)
+	return bounds
+	
